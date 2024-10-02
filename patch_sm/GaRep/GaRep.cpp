c2efcf5fa03317289f597f3cc723df3b38805e3f
mort13 <mo@redback.de>
2024-10-02 16:32:51 +0200

    v0.1 done

--- patch_sm/GaRep/GaRep.cpp
+++ patch_sm/GaRep/GaRep.cpp
@@ -10,16 +10,30 @@ Switch toggle;
 
 
 // GaRep variables
-float minorPentatonic[5] = {0, 3, 5, 7, 10};    // C, Eb, F, G, Bb
-int groundKey = 48;                             // Some key changes
+int minorPentatonic[25] = { 0,  3,  5,  7, 10,
+                            12, 15, 17, 19, 22,
+                            24, 27, 29, 31, 34,
+                            36, 39, 41, 43, 46,
+                            48, 51, 53, 55, 58};// C, Eb, F, G, Bb
+int lenScale = 5;
 int pow2[7] = {2,4,8,16,32,64,128};             // Lookup table for pow2
-int bufferLength[7] = {2,4,6,7,8,16,32};           // Bufferlength for CV values
-int cvBuffer[32] = {0};
+int bufferLength[8] = {2,4,6,7,8,12,16,16};           // Bufferlength for CV values
+int midiBuffer[16] = {0};
+int RHead = 0;
+int WHead = 0;
+int loopLength = 2;
+int counter = 0;
+bool sendnote = false;
 
-void mtocv(int midi)
+float mtocv(int midi)
 {
-    float cv = midi * 1/12;
-    return cv;
+    return static_cast<float>(midi) / 12.0f;;
+}
+
+float selectNote(int note, int root, int spread, int shift)
+{
+    int selNote = 5 + (note %spread) + shift;
+    return (minorPentatonic[selNote] + root);
 }
 
 void AudioCallback(AudioHandle::InputBuffer  in,
@@ -27,8 +41,64 @@ void AudioCallback(AudioHandle::InputBuffer  in,
                    size_t                    size)
 { 
     patch.ProcessAnalogControls();
-
     toggle.Debounce();
+    bool tr1 = patch.gate_in_1.Trig();
+    bool tr2 = patch.gate_in_2.Trig();
+    dsy_gpio_write(&patch.gate_out_1, patch.gate_in_1.State());
+
+
+    int pitchKnob = int(fmap(patch.GetAdcValue(CV_1),0,12));
+    float pitchCV = patch.GetAdcValue(CV_5)*12;
+    int pitch = int(DSY_CLAMP(pitchKnob+pitchCV,0,72));
+
+    int spreadKnob = int(fmap(patch.GetAdcValue(CV_2),1,2*lenScale));
+    int spreadCV = int(patch.GetAdcValue(CV_6)*2*(lenScale/5));
+    int spread = DSY_CLAMP(spreadKnob+spreadCV,0,2*lenScale);
+
+    int lengthKnob = 1+int(patch.GetAdcValue(CV_3)*8);
+    int lengthCV = 0;//int(fmap(DSY_CLAMP(patch.GetAdcValue(CV_7),0,5)/5,0,8));
+    int length = DSY_CLAMP(lengthKnob + lengthCV,0,8);
+    loopLength = bufferLength[length];
+
+    int shiftKnob = int(fmap(patch.GetAdcValue(CV_4),-lenScale,lenScale));
+    int shiftCV = int(patch.GetAdcValue(CV_8)*(lenScale/5));
+    int shift = DSY_CLAMP(shiftKnob+shiftCV,-lenScale,lenScale);
+
+    if (tr2)
+    {
+        RHead = 0;
+        WHead = 0;
+    }
+
+    if (tr1)
+    {
+        if (lengthKnob >= 8)
+        {
+            patch.WriteCvOut(2,5);
+            sendnote = true;
+            WHead = (WHead + 1) %loopLength;
+            midiBuffer[WHead] = rand()%5;
+            RHead = WHead;
+        }
+        else
+        {
+            RHead = (RHead +1) %loopLength;
+        }
+        float noteOut = selectNote(midiBuffer[RHead],pitch,spread,shift);
+        patch.WriteCvOut(1,mtocv(noteOut));
+    }
+    
+    if (sendnote&&counter < 100)
+    {
+        counter = (counter + 1);
+        patch.WriteCvOut(2,5-(counter/20));
+    }
+    else
+    {
+        counter = 0;
+        sendnote = false;
+        patch.WriteCvOut(2,0);
+    }
 
 }
 
--- patch_sm/GaRep/README.md
+++ patch_sm/GaRep/README.md
@@ -1,14 +1,28 @@
 # GaRep
 
-This example shows how to set up a simple reverb effect.
+This is my simple take of the Gamut Repetitor by Noise Engineering. When triggered it will generate random notes within a scale. Right now only minor pentatonic.
+In contrast to the Gamut Repetitor this wont generate notes below the root note, except when shifted. Notes are also held till the next trigger.
+Adding more scales via the switches is still a work in progress.
 
 ## Controls
+### Input
+| Input | Alternative | Function | Comment |
+| CV_1 | CV_5 | Root Note | Sets the lowest note of the scale |
+| CV_2 | CV_6 | Spread | Selects the amount of different notes above root note |
+| CV_3 | CV_7 | Bufferlength | Sets the length of the buffer, fully cw new random notes are fed into the buffer |
+| CV_4 | CV_8 | Shift | Shifts the selection of notes in scale 2 oct up or down |
+| Gate1_in | --- | In | Plays the next note in the buffer |
+| Gate2_in | --- | Reset | Resets the read and write position back to 0 |
+| Button | --- | --- | Unassigned |
+| Switch | --- | --- | Unassigned |
+| In_L | --- | --- | Unassigned |
+| In_R | --- | --- | Unassigned |
 
-| Pin Name | Pin Location | Function | Comment |
-| --- | --- | --- | --- |
-| CV_1 | C5 | Reverb Time | Controls the size of the reverb |
-| CV_2 | C4 | Reverb Dampening Frequency | Range is from 1kHz to 19kHz |
-| CV_3 | C3 | Input Level | Controls the amount of dry signal passing through to the output |
-| CV_4 | C2 | Send amount | Controls the amount of dry signal sent to the reverb |
+### Output
+| Gate1_out | --- | Gatethrough | Passes the gate through |
+| Gate2_out | --- | Gatethrough | Passes the gate through |
+| Out_L | --- | --- | Unassigned |
+| Out_R | --- | --- | Unassigned |
 
-The _comments.cpp file is identical to the main file, but has detailed comments explaining each piece of the source code.
+
+
